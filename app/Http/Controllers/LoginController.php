d651150dd980c20ee874794612c4653fc92c14fc
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-21 16:51:52 +0100

    add function redirectToGoogleProvider

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
-    //
+    public function redirectToGoogleProvider()
+{
+	$parameters = ['access_type' => 'offline'];
+	return Socialite::driver('google')->scopes(["https://www.googleapis.com/auth/drive"])->with($parameters)->redirect();
+}
 }
