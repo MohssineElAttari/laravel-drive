3170d7233bcc7bdd3c55605d48ff7093fbca42cb
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-21 17:37:55 +0100

    add construct in DriveController

--- app/Http/Controllers/DriveController.php
+++ app/Http/Controllers/DriveController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Facade\FlareClient\Stacktrace\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class DriveController extends Controller
 {
-    //
+    private $drive;
+    public function __construct(Google_Client $client)
+    {
+        $this->middleware(function ($request, $next) use ($client) {
+            $client->refreshToken(Auth::user()->refresh_token);
+            $this->drive = new Google_Service_Drive($client);
+            return $next($request);
+        });
+    }
+
+    
 }
