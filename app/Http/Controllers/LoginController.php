fc5128cb8386611da29b7ffce2a61923a7485bcb
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-21 16:53:50 +0100

    add function handleProviderGoogleCallback

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
     public function redirectToGoogleProvider()
-{
-	$parameters = ['access_type' => 'offline'];
-	return Socialite::driver('google')->scopes(["https://www.googleapis.com/auth/drive"])->with($parameters)->redirect();
-}
+    {
+        $parameters = ['access_type' => 'offline'];
+        return Socialite::driver('google')->scopes(["https://www.googleapis.com/auth/drive"])->with($parameters)->redirect();
+    }
+
+    public function handleProviderGoogleCallback()
+    {
+        $auth_user = Socialite::driver('google')->user();
+        $user = User::updateOrCreate(['email' => $auth_user->email], ['refresh_token' => $auth_user->token, 'name' => $auth_user->name]);
+        Auth::login($user, true);
+        return redirect()->to('/'); // Redirect to a secure page
+    }
 }
