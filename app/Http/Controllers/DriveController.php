017be1002c702f6e334d14c77fb9a40739bc8320
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-21 17:40:19 +0100

    add function listFolders in LoginController

--- app/Http/Controllers/DriveController.php
+++ app/Http/Controllers/DriveController.php
@@ -25,5 +25,27 @@ class DriveController extends Controller
         $this->ListFolders('root');
     }
 
-   
+    public function ListFolders($id)
+    {
+
+        $query = "mimeType='application/vnd.google-apps.folder' and '" . $id . "' in parents and trashed=false";
+
+        $optParams = [
+            'fields' => 'files(id, name)',
+            'q' => $query
+        ];
+
+        $results = $this->drive->files->listFiles($optParams);
+
+        if (count($results->getFiles()) == 0) {
+            print "No files found.\n";
+        } else {
+            print "Files:\n";
+            foreach ($results->getFiles() as $file) {
+                dump($file->getName(), $file->getID());
+            }
+        }
+    }
+
+    
 }
