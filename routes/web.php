ad5363c22d5f8d7aee5a1f9324e2ceb6d5a91577
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-21 16:46:44 +0100

    add routes login/google and login/google/callback

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login/google', 'Auth\LoginController@redirectToGoogleProvider');
+
+Route::get('login/google/callback', 'Auth\LoginController@handleProviderGoogleCallback');
