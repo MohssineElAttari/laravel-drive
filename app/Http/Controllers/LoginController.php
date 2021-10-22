dfc0da32e8f7f54eecc6557b05b81d8d10f222a5
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-22 10:37:42 +0100

    update unction handleProviderGoogleCallback in Login Controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,8 +20,24 @@ class LoginController extends Controller
 
     public function handleProviderGoogleCallback()
     {
+
         $auth_user = Socialite::driver('google')->user();
-        $user = User::updateOrCreate(['email' => $auth_user->email], ['refresh_token' => $auth_user->token, 'name' => $auth_user->name]);
+        $data = [
+            'token' => $auth_user->token,
+            'expires_in' => $auth_user->expiresIn,
+            'name' => $auth_user->name
+        ];
+        if($auth_user->refreshToken){
+            $data['refresh_token'] = $auth_user->refreshToken;
+        }
+ 
+        $user = User::updateOrCreate(
+            [
+                'email' => $auth_user->email
+            ],
+            $data
+        );
+ 
         Auth::login($user, true);
         return redirect()->to('/'); // Redirect to a secure page
     }
